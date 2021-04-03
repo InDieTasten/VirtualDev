831e4e575478aa435eee6efb4c4cc0d6c68db6b4
InDieTasten <indietasten@gmail.com>
2021-04-03 22:04:21 +0200

    Adjust SignalR chat to work between user and bot
    Closes #31

--- src/VirtualDev.Web/Hubs/ChatHub.cs
+++ src/VirtualDev.Web/Hubs/ChatHub.cs
@@ -7,9 +7,18 @@ namespace VirtualDev.Web.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private string CallerUserName => Context.User.FindFirst("urn:github:name")?.Value ?? "anonymous";
+
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // add to chat history
+            await Clients.Caller.SendAsync("ReceiveMessage", CallerUserName, message);
+
+            // processing by bot
+            string replyMessage = message;
+
+            // reply only to the sending user
+            await Clients.Caller.SendAsync("ReceiveMessage", "VirtualDev", replyMessage);
         }
     }
 }
